c78b1aaf0fb95b95869766759774172027816e16
ahmad43584 <95644078+ahmad43584@users.noreply.github.com>
2021-12-09 22:13:06 +0500

    Update spdf.js

--- plugins/spdf.js
+++ plugins/spdf.js
@@ -15,8 +15,8 @@ let toxicdevil = Config.WORKTYPE == 'public' ? false : true
 
     var webimage = await axios.get(`https://api.html2pdf.app/v1/generate?url=${match[1]}&apiKey=begC4dFAup1b8LyRXxAfjetfqDg2uYx8PWmh9YJ59tTZXiUyh2Vs72HdYQB68vyc`, { responseType: 'arraybuffer' })
 
-    await message.client.sendMessage(message.jid, '```Converting Site to PDF```', MessageType.text);
+    await message.client.sendMessage(message.jid, '```Converting Site to PDF please wait..```', MessageType.text);
 
-    await message.sendMessage(Buffer.from(webimage.data), MessageType.document, {mimetype: Mimetype.pdf, filename: 'Julie-Mwol.pdf'});
+    await message.sendMessage(Buffer.from(webimage.data), MessageType.document, {mimetype: Mimetype.pdf, filename: 'ahmad.pdf'});
 
     }));    
